d55b9df4d5cbbf4e83faeb5625ef1bb9a207d5e3
Arka Ariza <arjarizami@unal.edu.co>
2019-07-15 21:51:21 -0500

    ads

--- index.js
+++ index.js
@@ -2,27 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
-const passport  = require('passport');
+const passport = require('passport');
 const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
-
-mongoose.connect(
-    keys.mongoURI,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB Connected"))
+mongoose
+  .connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(
-    cookieSession({
-        maxAge:  30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 
 app.use(passport.initialize());
@@ -31,13 +28,13 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
