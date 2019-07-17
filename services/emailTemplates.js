c8a6b98bf36d07e4d2e09f504f47c8f0b55ac99e
Arka Ariza <arjarizami@unal.edu.co>
2019-07-17 11:48:14 -0500

    finally i think

--- services/emailTemplates.js
+++ services/emailTemplates.js
@@ -5,16 +5,16 @@ module.exports = survey => {
   <html>
     <body>
         <div style="text-align: center;">
-            <h3>I'd like your input!</h3>
-            <p>Please answer the following question:</p>
+            <h3>${survey.title}</h3>
+            <p>${survey.subject}</p>
             <p>${survey.body}</p>
             <div>
-                <a href="${keys.redirectDomain}/api/surveys/${
+                <a href="${keys.redirectDomain}api/surveys/${
     survey.id
   }/yes">Yes</a>
             </div>
             <div>
-                <a href="${keys.redirectDomain}/api/surveys/${
+                <a href="${keys.redirectDomain}api/surveys/${
     survey.id
   }/no">No</a>
             </div>
